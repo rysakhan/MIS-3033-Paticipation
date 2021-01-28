5885e2e878a8ece036c2bf01fa248ad49ad3f3b4
18438 <18438@DESKTOP-GGEKG2J>
2021-01-28 14:10:32 -0600

    keybpard malfunction

---
+++ 3033Review/StudentExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+/// <summary>
+/// @author Rysa 
+/// </summary>
+namespace StudentExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("SstudentdataRandom100Rows.csv");   
+           }
+    }
+}

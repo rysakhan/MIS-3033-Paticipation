140e5b9a8ae7a6bafdcd3612114dce5301f2d06b
18438 <18438@DESKTOP-GGEKG2J>
2021-02-11 14:57:00 -0600

    done :)

---
+++ WPF1/WPF1/EntryForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    public class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0000;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Address}) ({ZipCode})"; ;
+
+        }
+    }
+}

a09854df7787f0d4486ad4c28d9c6f06a4fd9e1e
18438 <18438@DESKTOP-GGEKG2J>
2021-03-07 20:39:41 -0600

    done

---
+++ Pokemon/Pokemon/InformationAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    public class InformationAPI
+    {
+        public string name { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+        public sprites sprites { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {name}, Height: {height}, Weight: {weight}";
+        }
+    }
+
+    public class sprites
+    {
+        public string front_default { get; set; }
+        public string back_default { get; set; }
+    }
+}
---
+++ Pokemon/Pokemon/Pokemon.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pokemon
+{
+    /// <summary>
+    /// Interaction logic for Pokemon.xaml
+    /// </summary>
+    public partial class Pokemon : Window
+    {
+        public Pokemon()
+        {
+            InitializeComponent();
+        }
+
+        InformationAPI info;
+
+        public void newWindow(InformationAPI info)
+        {
+            this.info = info;
+            img_pokemon.Source = new BitmapImage(new Uri(info.sprites.front_default));
+            img_pokemon.Source = new BitmapImage(new Uri(info.sprites.back_default));
+            txt_pokemonInfo.Text = info.ToString();
+        }
+
+        bool front = true;
+
+        private void btn_pic_Click(object sender, RoutedEventArgs e)
+        {
+            if (front == true)
+            {
+                img_pokemon.Source = new BitmapImage(new Uri(info.sprites.front_default));
+                front = false;
+            }
+            else
+            {
+                img_pokemon.Source = new BitmapImage(new Uri(info.sprites.back_default));
+                front = true;
+            }
+        }
+    }
+}

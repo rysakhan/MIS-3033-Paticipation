28cffe075e97c037f9cc0507b4862a77cdd0b153
18438 <18438@DESKTOP-GGEKG2J>
2021-03-11 13:18:04 -0600

    update

---
+++ ChuckNorrisJokes/ChuckNorrisJokes/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokes
+{
+    public class API
+    {
+
+        public string icon_url { get; set; }
+
+        public string id { get; set; }
+
+        public string url { get; set; }
+
+        public string value { get; set; }
+    }
+}
